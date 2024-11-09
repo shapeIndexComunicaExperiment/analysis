8fd3bc66951e167ab9efd5bd690054b36108fef3
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-11-09 09:38:35 +0100

    new results

--- generateDataset.py
+++ generateDataset.py
@@ -1,6 +1,6 @@
 import json
 from dataclasses import dataclass
-from typing import List, Dict, Tuple, Optional
+from typing import List, Dict, Tuple, Optional, Set
 
 
 @dataclass
@@ -10,9 +10,11 @@ class Dataset:
     numberHttpRequest: Dict[str, Optional[List[int]]]
     stdExecutionTime: Dict[str, Optional[List[float]]]
     executionTime: Dict[str,
-                               Dict[str, Optional[List[float]]]]
-    results: Dict[str, Dict[str, Optional[List[dict]]]]
+                        Dict[str, Optional[List[float]]]]
+    results: Dict[str, Dict[str, Optional[List[Set[dict]]]]]
     arrivalTimes: Dict[str, Dict[str, Optional[List[float]]]]
+    numberResults: Dict[str, Optional[List[int]]]
+
 
 def divideResultsIntoArrivalTime(results: List[dict]) -> Tuple[List[dict], List[float]]:
     resultsCurrated = []
@@ -22,6 +24,7 @@ def divideResultsIntoArrivalTime(results: List[dict]) -> Tuple[List[dict], List[
         arrivalTimes.append(result.pop('_arrival_time'))
         resultsCurrated.append(result)
     return (resultsCurrated, arrivalTimes)
+
 
 def generateDatasetFromResults(filepathFullResuls: str, filepathSummaryResuls: str, name: str) -> Dataset:
     fullResuls = None
@@ -33,6 +36,7 @@ def generateDatasetFromResults(filepathFullResuls: str, filepathSummaryResuls: s
     meanExecutionTime = {}
     numberHttpRequest = {}
     stdExecutionTime = {}
+    numberResults = {}
 
     with open(filepathFullResuls, 'rb') as rf:
         fullResuls = json.load(rf)
@@ -44,6 +48,7 @@ def generateDatasetFromResults(filepathFullResuls: str, filepathSummaryResuls: s
         meanExecutionTime[queryName] = []
         numberHttpRequest[queryName] = []
         stdExecutionTime[queryName] = []
+        numberResults[queryName] = []
         for version, data in versionData.items():
             for field, value in data.items():
                 if field == "timeout":
@@ -55,7 +60,8 @@ def generateDatasetFromResults(filepathFullResuls: str, filepathSummaryResuls: s
                 elif field == "execution_time":
                     meanExecutionTime[queryName].append(value["average"])
                     stdExecutionTime[queryName].append(value["std"])
-                
+                elif "n_results":
+                    numberResults[queryName].append(value)
 
     for queryName, versionData in fullResuls["data"].items():
         executionTime[queryName] = {}
@@ -69,8 +75,8 @@ def generateDatasetFromResults(filepathFullResuls: str, filepathSummaryResuls: s
                 if (type(repetition) != str):
                     res = divideResultsIntoArrivalTime(
                         repetition['results'])
-
-                    results[queryName][version].append(res[0])
+                    print(res[0])
+                    results[queryName][version].append(set(res[0]))
                     executionTime[queryName][version].append(
                         repetition['execution_time'])
                     arrivalTimes[queryName][version].append(res[1])
@@ -85,9 +91,12 @@ def generateDatasetFromResults(filepathFullResuls: str, filepathSummaryResuls: s
         executionTime=executionTime,
         meanExecutionTime=meanExecutionTime,
         numberHttpRequest=numberHttpRequest,
-        stdExecutionTime=stdExecutionTime)
+        stdExecutionTime=stdExecutionTime,
+        numberResults=numberResults
+        )
 
-def generateGroundTruthResults(filepath)-> List[dict]:
+
+def generateGroundTruthResults(filepath) -> List[dict]:
     data = None
     with open(filepath, 'rb') as rf:
         data = json.load(rf)
@@ -98,7 +107,7 @@ def generateGroundTruthResults(filepath)-> List[dict]:
         for key, value in binding.items():
             currentValue = '"{}"'.format(value['value'])
             if "datatype" in value:
-                currentValue+= '^^{}'.format(value["datatype"])
+                currentValue += '^^{}'.format(value["datatype"])
             transformedBinding[key] = currentValue
         resp.append(transformedBinding)
     return resp
